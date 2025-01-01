ede7848aac416ceffa7c383d508b5d67ee59bbda
Deepak <coderbouy92@gmail.com>
2025-01-02 00:36:13 +0530

    backend-url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const Config = {
-    HOST : "127.0.0.1",
+    HOST : "spotifybackend.ap-1.evennode.com/",
     PORT : 5000,
     DB_URL : "mongodb+srv://spotify:spotify@spotify.0qovp.mongodb.net/spotify"
 }
