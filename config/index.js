f06e13d5507fcac8e7cd3ef03d102992e018f37d
Deepak <coderbouy92@gmail.com>
2025-01-02 16:01:52 +0530

    host change

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const Config = {
-    HOST : "spotifybackend.ap-1.evennode.com/",
+    HOST : "127.0.0.0.1",
     PORT : 5000,
     DB_URL : "mongodb+srv://spotify:spotify@spotify.0qovp.mongodb.net/spotify"
 }
-module.exports = Config; 
+module.exports = Config;
