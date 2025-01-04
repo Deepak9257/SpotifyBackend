3796273b1e5ce7f74c0e7e8ccd546b5431f0e7c0
Deepak <coderbouy92@gmail.com>
2025-01-04 17:27:24 +0530

    DB URL added

--- config/index.js
+++ config/index.js
@@ -1,14 +1,9 @@
 
 require('dotenv').config()
 
-
-
-
-
-
 const Config = {
     HOST : process.env.HOST || "127.0.0.1",
     PORT : process.env.PORT || 5500,
-    DB_URL : process.env.DB_URL
+    DB_URL : process.env.DB_URL || 'mongodb+srv://spotify:spotify@spotify.0qovp.mongodb.net/spotify'
 }
 module.exports = Config;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
