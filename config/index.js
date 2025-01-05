d059221d99d3e1460ffc595c808495a890e51849
Deepak <coderbouy92@gmail.com>
2025-01-05 11:35:37 +0530

    DB_url

--- config/index.js
+++ config/index.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 
 const Config = {
+
+    
     HOST : process.env.HOST || "127.0.0.1",
     PORT : process.env.PORT || 5500,
     DB_URL : process.env.DB_URL || 'mongodb+srv://spotify:spotify@spotify.0qovp.mongodb.net/spotify'
