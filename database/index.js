98d1e51b5c43dae7d9c1598076a193f50e0e1ebc
Deepak <coderbouy92@gmail.com>
2025-01-06 20:21:58 +0530

    cleared DB url

--- database/index.js
+++ database/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const dbconn = () => {
 
     try {
-        const DB_URL = process.env.DB_URL || 'mongodb+srv://spotify:spotify@spotify.0qovp.mongodb.net/spotify'
+        const DB_URL = process.env.DB_URL
         mongoose.connect(DB_URL);
         const db = mongoose.connection;
         db.on('connected', () => {
