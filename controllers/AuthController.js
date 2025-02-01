2e98c74dca3ed22aa2174222ff09eadb06028a10
Deepak <coderbouy92@gmail.com>
2025-02-01 12:59:28 +0530

    fixing login error

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -43,7 +43,7 @@ const AuthController = {
         const token = generateJWT(Exist)
 
 
-        return res.json({status:true, msg:"Logined sucessfully", token, email} );
+        return res.json({status:true, message:"Logined sucessfully", token, email} );
 
 
     },
